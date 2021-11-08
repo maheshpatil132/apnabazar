2d541b48307d10fd6d72eb9ce557abbe88501c6c
maheshpatil132 <92781925+maheshpatil132@users.noreply.github.com>
2021-11-08 18:18:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ let navbar = document.getElementsByClassName('navbar')[0]
 let form = document.getElementsByClassName('search_form')[0]
 let slide = document.getElementsByClassName('slide')
 let slides = Array.from(document.getElementsByClassName('slide'))
+let bracket = document.getElementById('bracket')
+let bracket_2 = document.getElementById('bracket_2')
+let sub = document.getElementsByClassName('sub_link')[0]
+let sub_2 = document.getElementsByClassName('sub_link')[1]
+
 let index=0;
 btn.addEventListener('click', function(){
   navbar.classList.toggle('active')
@@ -25,4 +30,24 @@ setInterval(function(){
    }
    
  
-},2000)
+},2500)
+bracket.addEventListener('click',function(){
+  bracket.classList.toggle("fa-chevron-up")
+  if(bracket.classList.contains('fa-chevron-up')){
+     sub.style.display ="block"
+  }
+  else{
+    sub.style.display ="none"
+  }
+
+})
+bracket_2.addEventListener('click',function(){
+  bracket_2.classList.toggle("fa-chevron-up")
+  if(bracket_2.classList.contains('fa-chevron-up')){
+     sub_2.style.display ="block"
+  }
+  else{
+    sub_2.style.display ="none"
+  }
+
+})
