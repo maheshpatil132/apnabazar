47dc03d3a904bf45f56fa2ceff7ef19f16ab8c0a
maheshpatil132 <92781925+maheshpatil132@users.noreply.github.com>
2021-11-08 11:10:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,27 @@ let btn = document.getElementById('menu_btn')
 let search = document.getElementById('search')
 let navbar = document.getElementsByClassName('navbar')[0]
 let form = document.getElementsByClassName('search_form')[0]
+let slide = document.getElementsByClassName('slide')
+let slides = Array.from(document.getElementsByClassName('slide'))
+let index=0;
 btn.addEventListener('click', function(){
   navbar.classList.toggle('active')
 })
+
 search.addEventListener('click', function(){
   form.classList.toggle('active')
 })
+
+setInterval(function(){ 
+  slide[index].classList.remove('active')
+   index++
+   if(index<slides.length){
+    slide[index].classList.add('active')
+   }
+   else{
+     index=0;
+     slide[index].classList.add('active')
+   }
+   
+ 
+},2000)
