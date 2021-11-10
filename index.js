744ce6083a2515d3e9fd2d566e140f17761e6f95
maheshpatil132 <92781925+maheshpatil132@users.noreply.github.com>
2021-11-10 13:41:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,21 @@ let bracket = document.getElementById('bracket')
 let bracket_2 = document.getElementById('bracket_2')
 let sub = document.getElementsByClassName('sub_link')[0]
 let sub_2 = document.getElementsByClassName('sub_link')[1]
-
+let add = Array.from(document.getElementsByClassName("add"))
+let box = document.getElementsByClassName(" product_box")
+let cart = document.getElementsByClassName('cart')[0]
+console.log(box)
+console.log(add.length)
+add.forEach((element,index)=>{
+  element.addEventListener('click',function(){
+     let html =box[index]
+     localStorage.setItem("product",box[index].innerHTML)
+     console.log(html)
+  })
+  
+})
+let products =localStorage.getItem(`product`)
+ console.log(products) 
 let index=0;
 btn.addEventListener('click', function(){
   navbar.classList.toggle('active')
@@ -32,22 +46,13 @@ setInterval(function(){
  
 },2500)
 bracket.addEventListener('click',function(){
-  bracket.classList.toggle("fa-chevron-up")
-  if(bracket.classList.contains('fa-chevron-up')){
-     sub.style.display ="block"
+  bracket.classList.toggle("fa-rotate-180")
+  if(bracket.classList.contains('fa-rotate-180')){
+      sub.style.display ="block"
+     
   }
   else{
     sub.style.display ="none"
-  }
-
-})
-bracket_2.addEventListener('click',function(){
-  bracket_2.classList.toggle("fa-chevron-up")
-  if(bracket_2.classList.contains('fa-chevron-up')){
-     sub_2.style.display ="block"
-  }
-  else{
-    sub_2.style.display ="none"
   }
 
 })
